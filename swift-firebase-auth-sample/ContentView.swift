c506e370b7ad30f218ba010ad869826340e21aec
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2020-07-04 10:59:27 +0900

    Initial Commit

---
+++ swift-firebase-auth-sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-firebase-auth-sample
+//
+//  Created by 田中改 on 2020/07/04.
+//  Copyright © 2020 aratanaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
